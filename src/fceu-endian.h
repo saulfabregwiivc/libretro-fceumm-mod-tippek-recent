58b0074075a5964af74fba26cab8cedd453a63ad
saulfabreg Wii VC Project <saulfabregamboa@outlook.com>
2025-07-24 09:45:59 -0500

    Update fceu-endian.h

--- src/fceu-endian.h
+++ src/fceu-endian.h
@@ -13,6 +13,6 @@ int read32le(uint32 *Bufo, FILE *fp);
 void FlipByteOrder(uint8 *src, uint32 count);
 
 void FCEU_en32lsb(uint8 *, uint32);
-uint32 FCEU_de32lsb(uint8 *);
+uint32 FCEU_de32lsb(const uint8 *);
 
 #endif
